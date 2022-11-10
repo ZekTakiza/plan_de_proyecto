13a44ce344cf8f0575cce55a977746b910da369f
Tiza <36284736+ZekTakiza@users.noreply.github.com>
2022-11-10 15:51:58 +0000

    Código terminado

--- palanca.ino
+++ palanca.ino
@@ -1,80 +1,33 @@
-/*
- *  Project     Arduino XInput Library
- *  @author     David Madison
- *  @link       github.com/dmadison/ArduinoXInput
- *  @license    MIT - Copyright (c) 2019 David Madison
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- *  Example:      GamepadPins
- *  Description:  Uses all of the available pin inputs to build a 'complete'
- *                Xbox gamepad, with both analog joysticks, both triggers,
- *                and all of the main buttons.
- *
- *                * Joysticks should be your typical 10k dual potentiometers.
- *                  To prevent random values caused by floating inputs,
-                    joysticks are disabled by default.
- *                * Triggers can be either analog (pots) or digital (buttons).
- *                  Set the 'TriggerButtons' variable to change between the two.
- *                * Buttons use the internal pull-ups and should be connected
- *                  directly to ground.
- *
- *                These pins are designed around the Leonardo's layout. You
- *                may need to change the pin numbers if you're using a
- *                different board type
- *
- */
-
 #include <XInput.h>
 
-// Setup
-
-
-
-
-// Joystick Pins
-
-// Trigger Pins
-
-
-// Button Pins
 const int Pin_ButtonA = 1;
 const int Pin_ButtonB = 3;
 const int Pin_ButtonX = 5;
 const int Pin_ButtonY = 7;
-
 const int Pin_ButtonLB = 9;
 const int Pin_ButtonRB = 11;
-
 const int Pin_ButtonStart = 13;
 
-// Directional Pad Pins
 const int Pin_DpadUp    = A0;
 const int Pin_DpadDown  = A1;
 const int Pin_DpadLeft  = A2;
 const int Pin_DpadRight = A3;
 
 void setup() {
-	// If using buttons for the triggers, use internal pull-up resistors
+	pinMode(2, OUTPUT);
+	pinMode(4, OUTPUT);
+	pinMode(6, OUTPUT);
+	pinMode(8, OUTPUT);
+	pinMode(10, OUTPUT);
+	pinMode(12, OUTPUT);
 	
-
-	// Set buttons as inputs, using internal pull-up resistors
+	digitalWrite(2, LOW);
+	digitalWrite(4, LOW);
+	digitalWrite(6, LOW);
+	digitalWrite(8, LOW);
+	digitalWrite(10, LOW);
+	digitalWrite(12, LOW);
+	
 	pinMode(Pin_ButtonA, INPUT_PULLUP);
 	pinMode(Pin_ButtonB, INPUT_PULLUP);
 	pinMode(Pin_ButtonX, INPUT_PULLUP);
@@ -83,25 +36,19 @@ void setup() {
 	pinMode(Pin_ButtonLB, INPUT_PULLUP);
 	pinMode(Pin_ButtonRB, INPUT_PULLUP);
 
-	pinMode(Pin_ButtonBack, INPUT_PULLUP);
 	pinMode(Pin_ButtonStart, INPUT_PULLUP);
-
-	pinMode(Pin_ButtonL3, INPUT_PULLUP);
-	pinMode(Pin_ButtonR3, INPUT_PULLUP);
 
 	pinMode(Pin_DpadUp, INPUT_PULLUP);
 	pinMode(Pin_DpadDown, INPUT_PULLUP);
 	pinMode(Pin_DpadLeft, INPUT_PULLUP);
 	pinMode(Pin_DpadRight, INPUT_PULLUP);
 
-	XInput.setAutoSend(false);  // Wait for all controls before sending
+	XInput.setAutoSend(false); 
 
 	XInput.begin();
 }
 
 void loop() {
-	// Read pin values and store in variables
-	// (Note the "!" to invert the state, because LOW = pressed)
 	boolean buttonA = !digitalRead(Pin_ButtonA);
 	boolean buttonB = !digitalRead(Pin_ButtonB);
 	boolean buttonX = !digitalRead(Pin_ButtonX);
@@ -118,7 +65,7 @@ void loop() {
 	boolean dpadLeft  = !digitalRead(Pin_DpadLeft);
 	boolean dpadRight = !digitalRead(Pin_DpadRight);
 
-	// Set XInput buttons
+	
 	XInput.setButton(BUTTON_A, buttonA);
 	XInput.setButton(BUTTON_B, buttonB);
 	XInput.setButton(BUTTON_X, buttonX);
@@ -130,8 +77,8 @@ void loop() {
 	XInput.setButton(BUTTON_START, buttonStart);
 
 
-	// Set XInput DPAD values
+	
 	XInput.setDpad(dpadUp, dpadDown, dpadLeft, dpadRight);
-	// Send control data to the computer
+	
 	XInput.send();
 }
